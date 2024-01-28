0421c7b0e3376bd4473ee0c17930a89b55aac90f
yuelong <34259594+YuelongWu@users.noreply.github.com>
2024-01-27 22:26:10 -0500

    Update msemQualityMapViewer.m

--- msemQualityMapViewer.m
+++ msemQualityMapViewer.m
@@ -2,11 +2,9 @@ addpath('PlotIMQA');
 try
     addpath('ConfigFiles');
     load('mSEM_retake_manager_default_folder.mat')
-    result_dir = 'Z:\lichtmanfs2\Yuelong\RetakeManagerVer2Beta\Results\U19_Zebrafish';
     result_dir = uigetdir(result_dir,'Select the result folder');
 catch
-    result_dir = '';
-    result_dir = 'Z:\lichtmanfs2\Yuelong\RetakeManagerVer2Beta\Results\U19_Zebrafish';
+    result_dir = '.';
     result_dir = uigetdir(result_dir,'Select the result folder');
 end
 if isnumeric(result_dir)
